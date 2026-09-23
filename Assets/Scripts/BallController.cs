2f9c601868ca91c207c6f3adcae93b8c3c2556cf
chocowriter <chocowriter0@gmail.com>
2026-09-23 11:30:08 +0900

    move ball

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -3,10 +3,33 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class BallController : MonoBehaviour
 {
+    [SerializeField] private float speed = 6f;
+    [SerializeField] private Vector2 startDirection = new Vector2(1f, 1f);
+
     private Rigidbody2D rb;
+    private Vector3 startPosition;
+
+    public float Speed => speed;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        startPosition = transform.position;
+    }
+
+    private void Start()
+    {
+        Launch();
+    }
+
+    public void Launch()
+    {
+        rb.linearVelocity = startDirection.normalized * speed;
+    }
+
+    public void ResetBall()
+    {
+        rb.linearVelocity = Vector2.zero;
+        transform.position = startPosition;
     }
 }
--- Assets/Scripts/PaddleController.cs
+++ Assets/Scripts/PaddleController.cs
@@ -2,5 +2,50 @@ using UnityEngine;
 
 public class PaddleController : MonoBehaviour
 {
-    
+    [SerializeField] private float moveSpeed = 8f;
+    [SerializeField] private float minX = -7.5f;
+    [SerializeField] private float maxX = 7.5f;
+
+    private Vector3 startPosition;
+    private SpriteRenderer spriteRenderer;
+
+    public float Width
+    {
+        get
+        {
+            if (spriteRenderer == null)
+            {
+                return 1f;
+            }
+
+            return spriteRenderer.bounds.size.x;
+        }
+    }
+
+    private void Awake()
+    {
+        startPosition = transform.position;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        float input = Input.GetAxisRaw("Horizontal");
+        Vector3 movement = Vector3.right * input * moveSpeed * Time.deltaTime;
+        transform.position += movement;
+
+        ClampPosition();
+    }
+
+    private void ClampPosition()
+    {
+        Vector3 position = transform.position;
+        position.x = Mathf.Clamp(position.x, minX, maxX);
+        transform.position = position;
+    }
+
+    public void ResetPaddle()
+    {
+        transform.position = startPosition;
+    }
 }
